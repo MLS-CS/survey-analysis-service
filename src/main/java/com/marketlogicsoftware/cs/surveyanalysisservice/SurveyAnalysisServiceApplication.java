e9bfb502016477493db2c35b2289b4e572dda2c3
Anant Pawar <anant.c.pawar@gmail.com>
2019-09-08 23:00:40 +0530

    initalized

---
+++ src/main/java/com/marketlogicsoftware/cs/surveyanalysisservice/SurveyAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marketlogicsoftware.cs.surveyanalysisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyAnalysisServiceApplication.class, args);
+	}
+
+}
